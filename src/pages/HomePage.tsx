d06c415fbca4364bc50ba4186a8d8321ec4f6fc2
Hiroshi Kadokura <abbiscuit.com@gmail.com>
2022-04-24 19:16:47 +0900

    fix: semantics

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -10,11 +10,11 @@ function HomePage(): JSX.Element {
         <article className="bg-white">
           <section>
             <header className="bg-gradient-to-r from-blue-700 to-blue-300">
-              <div className="space-y-4 px-4 py-4">
+              <div className="px-4 py-4">
                 <h2 className="text-center text-2xl font-semibold text-white">
                   何をデザインしますか？
                 </h2>
-                <form>
+                <form className="mt-4">
                   <input
                     className="relative w-full px-2 py-2"
                     type="text"
@@ -42,43 +42,45 @@ function HomePage(): JSX.Element {
           </section>
         </article>
 
-        <section className="space-y-20 px-4 py-4">
-          <section>
-            <div className="h-40 w-full bg-blue-500"></div>
-            <h3 className="mt-4 text-lg font-semibold">早速アイデアを形に</h3>
-            <p className="leading-relaxed">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
-              soluta minima
-            </p>
-            <button className="mt-4 bg-purple-500 px-6 py-2 text-sm text-white">
-              デザインをはじめる
-            </button>
-          </section>
-          <section>
+        <div className="space-y-20 py-4">
+          <article>
+            <div>
+              <div className="h-40 w-full bg-blue-500"></div>
+              <h3 className="mt-4 text-lg font-semibold">早速アイデアを形に</h3>
+              <p className="mt-2 leading-relaxed">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
+                soluta minima
+              </p>
+              <button className="mt-4 bg-purple-500 px-6 py-2 text-sm text-white">
+                デザインをはじめる
+              </button>
+            </div>
+          </article>
+          <article>
             <div className="h-40 w-full bg-blue-500"></div>
             <h3 className="text-lg font-semibold">簡単に共同作業</h3>
-            <p className="leading-relaxed">
+            <p className="mt-2 leading-relaxed">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
               soluta minima
             </p>
             <button className="mt-4 bg-purple-500 px-6 py-2 text-sm text-white">
               デザインをはじめる
             </button>
-          </section>
-          <section>
+          </article>
+          <article>
             <div className="h-40 w-full bg-blue-500"></div>
             <h3 className="text-lg font-semibold">
               簡単に共同自信を持って公開する作業
             </h3>
-            <p className="leading-relaxed">
+            <p className="mt-2 leading-relaxed">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit,
               soluta minima
             </p>
             <button className="mt-4 bg-purple-500 px-6 py-2 text-sm text-white">
               デザインをはじめる
             </button>
-          </section>
-        </section>
+          </article>
+        </div>
       </main>
     </div>
   )
